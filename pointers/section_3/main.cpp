f202a992e075ecfc2c3511fbcb7d032d0d3e2068
thisam90 <103902607+thisam90@users.noreply.github.com>
2025-10-06 15:14:35 +0700

    Add section 4 pointer example and update section 3
    Added a new file for section 4 demonstrating pointer dereferencing. Updated section 3 main.cpp with additional pointer manipulation examples for player health and score. Updated .gitignore to exclude section_4 Xcode project files.

--- pointers/section_3/main.cpp
+++ pointers/section_3/main.cpp
@@ -20,6 +20,23 @@ int main(){
     int *iptr = &num;
     std::cout <<"Value address:   " << &num << std::endl;
     std::cout <<"Pointer address: " << iptr << std::endl;
+        
+    int player_health = 100;
+    int *hptr = &player_health;
+    std::cout << "Players health: " << player_health << std::endl;
+    std::cout << "Damage 10 points ... " << std::endl;
+    *hptr -= 10;
+    std::cout << "Players health: " << player_health << std::endl;
+    
+    
+    
+    int score = 90;
+    int *sptr = nullptr;
+    sptr = &score;
+    std::cout << "Score: " << score << std::endl;
+    std::cout << "Score 10+ ptrs \n";
+    *sptr += 10;
+    std::cout << "Score: " << score << std::endl;
     
     return 0;
 }
---
+++ pointers/section_4/section_4.cpp
@@ -0,0 +1,18 @@
+/**********************************************
+ * Project   : Section 4 - Dereferencing Pointers
+ * File      : section_4.cpp
+ * Author    : Alexandru Lucian Manolache
+ * Created   : 06 10 2025
+ * Copyright : (c) 2025 by Alexandru Lucian Manolache
+ *
+ * Description:
+ *  Dereferencing Pointers
+ **********************************************/
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
