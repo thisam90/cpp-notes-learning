aca8d4fc5efa5e0150f1a973f1022abde934c908
thisam90 <103902607+thisam90@users.noreply.github.com>
2025-10-06 23:13:18 +0700

    Add Makefiles and code for sections 7 and 8
    Added Makefile and initial code for section 7, demonstrating pointer and array relationships, and created Makefile and skeleton for section 8. Updated section_7.cpp with author information and new examples on pointer and array usage.

--- pointers/section_7/section_7.cpp
+++ pointers/section_7/section_7.cpp
@@ -1,9 +1,9 @@
 /**********************************************
  * Project   : Section 7
  * File      : section_7.cpp
- * Author    : Your Name
+ * Author    : Alex Manolache   
  * Created   : 06 10 2025
- * Copyright : (c) 2025 by Your Name
+ * Copyright : (c) 2025 by Alex Manolache 
  *
  * Description:
  *   Relationship between pointer and arrays
@@ -14,6 +14,35 @@
 
  int main(){
 
+    //MARK: How do you declare an array?
+    int playerscores[5] = {40,80,23,56,89};
 
+    //MARK: How do you access elements?
+    std::cout << playerscores[0] << " first element "   << std::endl;
+    std::cout << playerscores[1] << " second element "  << std::endl;
+
+    int num = 42;
+    int *ptr = &num; // address of num
+
+    std::cout << ptr << " - address of num " << std::endl;
+    std::cout << *ptr << " - dereferencing the pointer " << std::endl;
+
+    int scores[] = {100,95,89};
+    int *score_ptr = scores;
+
+    std::cout << scores + 0 << std::endl; // 0x16b1e3110
+    std::cout << scores + 1 << std::endl; // 0x16b1e3114
+    std::cout << scores + 2 << std::endl; // 0x16b1e3118
+
+    std::cout << " ========== " << std::endl;
+    std::cout << *(scores + 0) << std::endl; // 100
+    std::cout << *(scores + 1) << std::endl;
+    std::cout << *(scores + 2) << std::endl;
+
+    /* General rule: array[index] = *(array + index) */
+
+    
+
+    
     return 0;
  }
---
+++ pointers/section_8/section_8.cpp
@@ -0,0 +1,18 @@
+/**********************************************
+ * Project   : Section 8
+ * File      : section_8.cpp
+ * Author    : Alex Manolache
+ * Created   : 06 10 2025
+ * Copyright : (c) 2025 by Alex Manolache
+ *
+ * Description:
+ *   Pass-by-Reference with Pointers
+ **********************************************/
+
+
+int main(){
+    
+
+
+    return 0;
+}
