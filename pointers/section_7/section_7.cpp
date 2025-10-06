de8887631cabbf46340edfe92e209eb3378d016c
thisam90 <103902607+thisam90@users.noreply.github.com>
2025-10-06 20:53:25 +0700

    Add .gitignore for section_6 and create section_7.cpp with initial documentation

---
+++ pointers/section_7/section_7.cpp
@@ -0,0 +1,10 @@
+/**********************************************
+ * Project   : Section 7
+ * File      : section_7.cpp
+ * Author    : Your Name
+ * Created   : 06 10 2025
+ * Copyright : (c) 2025 by Your Name
+ *
+ * Description:
+ *   Relationship between pointer and arrays
+ **********************************************/
